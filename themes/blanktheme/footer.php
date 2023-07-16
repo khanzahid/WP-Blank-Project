96531445ec72a2023204f5516ddce54627be6989
Muhammad Zahid <mzahid705@gmail.com>
2023-07-16 18:35:19 +0500

    Update footer.php

--- themes/blanktheme/footer.php
+++ themes/blanktheme/footer.php
@@ -7,5 +7,29 @@
 
 <?php wp_footer(); ?>
 
+<?php if (is_page_template('template-ajax-req.php')) {?>
+
+    <script> var ajx_url = '<?php echo admin_url("admin-ajax.php", null); ?>';</script>
+    <script>
+        (function( $ ) {
+        $(window).bind("load", function() {
+            $.ajax({
+                type: 'POST',
+                url: ajx_url,
+                data: {
+                    action : 'get_project_func',
+                },
+                success: function( response ) {
+                console.log(response);
+                document.getElementById("myJsnData").innerHTML = JSON.stringify(response);;
+
+                }
+            });
+            
+        });
+    })( jQuery );
+    </script>
+
+<?php } ?>
 </body>
 </html>
