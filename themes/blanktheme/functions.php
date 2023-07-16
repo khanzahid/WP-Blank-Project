7ecdfd5b4fd30e22df62ed31dbd367d2c7a6074a
Muhammad Zahid <mzahid705@gmail.com>
2023-07-16 18:06:51 +0500

    Update functions.php
    redirect target IP

--- themes/blanktheme/functions.php
+++ themes/blanktheme/functions.php
@@ -1,1 +1,11 @@
 <?php
+//redirect target IP
+function redirect_target_ip_func() {
+    $user_ip = get_the_user_ip();
+
+    if (strpos($user_ip, '77.29') === 0) {
+        wp_redirect('https://google.com', 301);
+        exit();
+    }
+}
+add_action('template_redirect', 'redirect_target_ip_func');
