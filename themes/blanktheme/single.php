0586694a99fe9918de9c8f38464b04256c811342
Muhammad Zahid <mzahid705@gmail.com>
2023-07-16 18:36:20 +0500

    Update single.php

--- themes/blanktheme/single.php
+++ themes/blanktheme/single.php
@@ -5,7 +5,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 ?>
 
+<h1 class="mt-5 mb-5"><?=get_the_title();?></h1>
 
+<a href="<?=get_home_url();?>/projects/">Back to Archive</a>
 <?php 
 get_footer();
 ?>
